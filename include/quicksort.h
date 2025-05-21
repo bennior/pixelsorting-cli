615379d3ecf033b3663da7c262128d93eaec81d6
bennior <benni.hd@icloud.com>
2025-05-21 13:14:15 +0200

    Removed partition() declaration from quicksort.h

--- include/quicksort.h
+++ include/quicksort.h
@@ -1,7 +1,6 @@
 #ifndef _QUICKSORT_
 #define _QUICKSORT_
 
-void partition(char* array, int low, int high, int desired_channels, float (*hbs)(char, char, char));
 void quicksort(char* array, int length, int desired_channels, float (*hbs)(char, char, char));
 
 #endif

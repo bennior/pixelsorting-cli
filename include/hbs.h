b985fccfe95eea237e33172a173cd8bf3dcae755
bennior <benni.hd@icloud.com>
2025-05-21 13:11:20 +0200

    Removed unnecessary function declarations from header files

--- include/hbs.h
+++ include/hbs.h
@@ -1,8 +1,6 @@
 #ifndef _HBS_
 #define _HBS_
 
-float get_max(float* arr, int size);
-float get_min(float* arr, int size);
 float get_hue(char r, char g, char b);
 float get_saturation(char r, char g, char b);
 float get_brightness(char r, char g, char b);

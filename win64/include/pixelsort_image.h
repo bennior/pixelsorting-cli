45888b1a8ac4b2e224c6d822d647cd29fbdb05a1
bennior <benni.hd@icloud.com>
2025-06-26 20:04:01 +0200

    Applied changes to CMakeLists.txt

--- win64/include/pixelsort_image.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PIXELSORT_IMAGE_
-#define _PIXELSORT_IMAGE_
-
-#include "pixel_stream_context.h"
-
-void pixelsort_image(const char* input, const char* output, void (*mask_ptr)(char*, pixel_stream_context*, int*, int, int, int), void (*rotate_image)(char*, int, int, int), float (*hbs)(char, char, char));
-
-#endif
--- win64/include/pixelsort_video.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PIXELSORT_VIDEO_
-#define _PIXELSORT_VIDEO_
-
-#include "pixel_stream_context.h"
-
-void pixelsort_video(const char* input, const char* output, void (*mask_ptr)(char*, pixel_stream_context*, int*, int, int, int), void (*rotate_image)(char*, int, int, int), float (*hbs)(char, char, char));
-
-#endif
